d83f1be44f2fe5e185b3de16972702d613b990dc
Hoang Trong Bao <baoht@baoht-workstation.zigexn.vn>
2017-08-10 11:20:01 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
   # get 'static_pages/contact'
   # get  '/help',    to: 'static_pages#help', as: 'helf'
 
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+  get  'help',    to: 'static_pages#help'
+  get  'about',   to: 'static_pages#about'
+  get  'contact', to: 'static_pages#contact'
   get  'signup',   to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
